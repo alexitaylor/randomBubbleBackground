11d3bb71d00441876e7349837352e0834ab5f935
alexitaylor <alexi@bluesunrise.com>
2017-06-28 13:53:30 -0700

    Clean up code

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -13,14 +13,12 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
       bubbles: {
         left: 23,
         top: 23,
         width: 200,
         height: 200,
       },
-      bubbleArrayClass: [],
     };
   },
   methods: {
@@ -49,23 +47,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 
 /*=====================
  Bubbles Styles

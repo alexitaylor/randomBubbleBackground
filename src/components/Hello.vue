9a66ae20b26512e03d2a05b8e2645651306fe9b2
alexitaylor <alexi@bluesunrise.com>
2017-06-28 12:52:04 -0700

    Added color to Background and shadow to circles

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section>
-      <div v-for="n  in 1500" :style="bubbleClass(n)"></div>
+      <div v-for="n  in 1000" :style="bubbleClass(n)" :class="zDepth()"></div>
     </section>
   </div>
 </template>
@@ -39,6 +39,10 @@ export default {
       };
       return bubble;
     },
+    zDepth() {
+      const z = _.random(1, 5);
+      return `z-depth-${z}`;
+    },
   },
 };
 </script>
@@ -72,7 +76,7 @@ section {
   top : 0px;
   width : 595px;
   height : 400px;
-  background: #fff;
+  background: #FFFFFE;
 }
 
 @-webkit-keyframes scale-up {
@@ -173,6 +177,30 @@ div {
   -ms-animation: scale-up infinite ease-in-out;
   animation: scale-up infinite ease-in-out;
   border-radius: 50%;
+}
+
+.z-depth-1 {
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+}
+
+.z-depth-1-half {
+  box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15);
+}
+
+.z-depth-2 {
+  box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.z-depth-3 {
+  box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+
+.z-depth-4 {
+  box-shadow: 0 16px 28px 0 rgba(0, 0, 0, 0.22), 0 25px 55px 0 rgba(0, 0, 0, 0.21);
+}
+
+.z-depth-5 {
+  box-shadow: 0 27px 24px 0 rgba(0, 0, 0, 0.2), 0 40px 77px 0 rgba(0, 0, 0, 0.22);
 }
 
 </style>
